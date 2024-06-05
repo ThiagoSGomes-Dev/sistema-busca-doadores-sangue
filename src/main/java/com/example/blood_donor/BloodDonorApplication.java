ca02e34c2696a962723e91ea20631749241fb021
ThiagoSGomes-Dev <thiagodos.10@gmail.com>
2024-06-05 20:52:20 -0300

    test-6 produção arquivo

--- src/main/java/com/example/blood_donor/BloodDonorApplication.java
+++ src/main/java/com/example/blood_donor/BloodDonorApplication.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BloodDonorApplication {
 
 	public static void main(String[] args) {
+		try {
+			// Adiciona um atraso de 3 segundos
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
 		SpringApplication.run(BloodDonorApplication.class, args);
 	}
-
 }
